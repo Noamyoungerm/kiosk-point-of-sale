eecbacb0ccb783c2a3d5bbcaa607a39fc97cac2d
Noamyoungerm <noamyoungerm@gmail.com>
2014-08-19 16:32:29 +0300

    Added additional information restrictions on non-admins

--- server/publications.js
+++ server/publications.js
@@ -1,13 +1,20 @@
 Meteor.publish('transactions', function(){
-    return Transactions.find();
+    if(Roles.userIsInRole(this.userId, ['admin', 'finance-management'])) {
+		return Transactions.find();
+	}
+	this.stop();
+    return;
 });
 
 Meteor.publish('items', function(){
-    return Items.find();
+    if(Roles.userIsInRole(this.userId, ['admin', 'inventory-management'])) {
+	    return Items.find();
+	}
+	return Items.find({}, {fields: {name: 1, saleprice: 1, qty: 1}});
 });
 
 Meteor.publish('users', function(){
-  if(Roles.userIsInRole(this.userId, 'admin')) { //Publish the student's own data instead
+  if(Roles.userIsInRole(this.userId, 'admin')) {
     return Meteor.users.find();
   }
   this.stop();
